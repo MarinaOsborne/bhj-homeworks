4deba06428424e40d630f2f7e957aa18e5b2e335
Marina Osborne <65596875+MarinaOsborne@users.noreply.github.com>
2020-10-14 17:04:28 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,19 @@
 'use strict'
 
-const cross = document.getElementsByClassName('modal__close modal__close_times');
+const crosses = document.getElementsByClassName('modal__close modal__close_times');
 const main = document.getElementById("modal_main");
 const success = document.getElementById("modal_success");
 const firstButton = document.getElementsByClassName('btn btn_danger modal__close show-success');
 
+for (let cross of crosses) {
+    
+    cross.onclick = function() { 
 
-cross[0].onclick = function() { 
-      main.className = "modal";    
+    this.closest("div.modal").classList.remove("modal_active")
+      
+    }
 }
 
-cross[1].onclick = function() { 
-    success.className = "modal";  
-}
 
 firstButton[0].onclick = function() { 
     main.className = "modal";
