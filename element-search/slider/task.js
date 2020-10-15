8681799797dea9f3279c866ee29eb055ead93b15
Marina Osborne <65596875+MarinaOsborne@users.noreply.github.com>
2020-10-15 15:35:49 +0300

    Update task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -8,40 +8,42 @@ const sliderDots = document.getElementsByClassName('slider__dot');
 const arrDots = Array.from(sliderDots);
 
 
-let slideIndex = 1;
+
+
+let active = arrSlides.findIndex(item => item.classList.contains("slider__item_active"));
 
 function showSlides(n) {
-  
-    if (n > sliderItems.length) {
-        slideIndex = 1
+   
+    if (n > sliderItems.length - 1 ) {
+        active = 0
       }
 
-    if (n < 1) {
-        slideIndex = sliderItems.length
+    if (n < 0) {
+        active = sliderItems.length -1
     } 
+    
+    for (let slide of arrSlides) {
+        slide.classList.remove("slider__item_active"); 
+      }
+      arrSlides[active].classList.add("slider__item_active");   
 
-    let active = arrSlides.findIndex(item => item.classList.contains("slider__item_active"));
-    
-    arrSlides[slideIndex - 1].classList.toggle("slider__item_active"); 
-    arrSlides[active].classList.toggle("slider__item_active");   
-    
-    arrDots[slideIndex - 1].classList.toggle("slider__dot_active"); 
-    arrDots[active].classList.remove("slider__dot_active"); 
+    for (let dot of arrDots) {
+        dot.classList.remove("slider__dot_active"); 
+      }
+      arrDots[active].classList.add("slider__dot_active");   
  
 }
 
 
-
 sliderArrow[0].onclick = function() { 
-
-    showSlides(slideIndex -= 1);  
+    
+    showSlides(active -= 1);  
     
 }   
  
-
 sliderArrow[1].onclick = function() { 
 
-    showSlides(slideIndex += 1);  
+    showSlides(active += 1);  
 } 
 
 
@@ -49,16 +51,31 @@ sliderArrow[1].onclick = function() {
     
      sliderDots[i].onclick = function() { 
 
-        sliderDots[i].classList.toggle("slider__dot_active"); 
+        for (let dot of arrDots) {
 
-        let activeSlide = arrSlides.findIndex(item => item.classList.contains("slider__item_active"));
+          dot.classList.remove("slider__dot_active"); 
         
-        arrSlides[activeSlide].classList.toggle("slider__item_active");  
+        }
 
-        arrSlides[i].classList.toggle("slider__item_active"); 
+        for (let slide of arrSlides) {
+
+          slide.classList.remove("slider__item_active"); 
+        }
+    
+        arrDots[i].classList.toggle("slider__dot_active"); 
         
-        arrDots[activeSlide].classList.remove("slider__dot_active"); 
-   
+        arrSlides[i].classList.toggle("slider__item_active");  
 
+        active = i;
+        
      } 
  } 
+
+
+
+
+
+ 
+
+
+
