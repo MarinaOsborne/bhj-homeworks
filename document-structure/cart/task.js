d8c93f00d8e95537ce7f9d3ca11a56587a68830f
Marina Osborne <65596875+MarinaOsborne@users.noreply.github.com>
2020-11-13 16:10:19 +0300

    Update task.js

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,71 +1,61 @@
 'use strict'
 
-const productAdds = document.getElementsByClassName('product__add');
-const quantity = document.getElementsByClassName('product__quantity-value');
-const cartQuantity = document.getElementsByClassName('cart__product-count');
-const img = document.getElementsByClassName('product__image');
-const id = Array.from(document.getElementsByClassName('product'));
-const cartProducts = document.getElementsByClassName('cart__products');
 const cartProduct = document.getElementsByClassName('cart__product');
 
 const buttons = document.getElementsByClassName('product__quantity-controls');
-// console.log(buttons)
+
+const products = Array.from(document.getElementsByClassName('product'));
 
 
-for (let button of buttons) {
+for (let product of products) {
 
-  button.addEventListener('click', event => {
+  product.addEventListener('click', event => {
 
     let arrProd = Array.from(cartProduct);
 
     let findCart = arrProd.find(item => 
+
       item.dataset.id == event.target.closest('.product').dataset.id)
-    
-    
-
-    if (event.target.classList.contains("product__quantity-control_dec") && findCart && +findCart.lastElementChild.textContent > 0) {
-
-      findCart.lastElementChild.textContent = +findCart.lastElementChild.textContent - 1 
-
-       } else if (event.target.classList.contains("product__quantity-control_dec") && !findCart && +event.target.nextElementSibling.textContent > 0) {
        
-          event.target.nextElementSibling.textContent -= 1;
+
+      if (event.target.classList.contains("product__quantity-control_dec") && findCart && +findCart.lastElementChild.textContent > 1) {
+
+                findCart.lastElementChild.textContent -=1;
+          
+                 } else if (event.target.classList.contains("product__quantity-control_dec") && !findCart && +event.target.nextElementSibling.textContent > 1) {
+                 
+                    event.target.nextElementSibling.textContent -= 1;
+                  }
+
+      if (event.target.classList.contains("product__quantity-control_inc") && findCart) {
+
+        findCart.lastElementChild.textContent = +findCart.lastElementChild.textContent + 1 
+
+         } else if (event.target.classList.contains("product__quantity-control_inc") && !findCart) {
+
+            let currentNumber = +event.target.previousElementSibling.textContent;  
+            currentNumber += 1;
+            event.target.previousElementSibling.textContent = currentNumber
+
+      } 
+
+
+    const cartProd = document.querySelector('.cart__products');
+    let quantity = event.target.parentElement.querySelector('.product__quantity-value')
+
+    if (!findCart && event.target.classList.contains("product__add")) {
+
+      cartProd.innerHTML += 
+      `<div class="cart__product" data-id=${event.target.closest('.product').dataset.id}>
+          <img class="cart__product-image" src=${event.target.closest('.product').firstElementChild.nextElementSibling.getAttribute('src')}>
+          <div class="cart__product-count">${quantity.innerText}</div>
+      </div>`  } else if (findCart && event.target.classList.contains("product__add")) {
+
+        findCart.lastElementChild.textContent = +findCart.lastElementChild.textContent + 1 
     }
-
-
-    if (event.target.classList.contains("product__quantity-control_inc") && findCart) {
-
-      findCart.lastElementChild.textContent = +findCart.lastElementChild.textContent + 1 
-
-      } else if (event.target.classList.contains("product__quantity-control_inc") && !findCart) {
-
-        let currentNumber = +event.target.previousElementSibling.textContent;  
-        currentNumber += 1;
-        event.target.previousElementSibling.textContent = currentNumber
-
-   } 
 
   })
+
 }
- 
 
-for (let i = 0; i < productAdds.length; i++ ) {
 
-    productAdds[i].addEventListener('click', event => { 
-
-      let cartValue =  +quantity[i].textContent; 
-      let src = img[i].getAttribute('src');
-      let itemId = id[i].dataset.id;        
-           
-    for (let cartProduct of cartProducts) {
-        cartProduct.innerHTML += 
-
-            `<div class="cart__product" data-id="${itemId}">
-            <img class="cart__product-image" src="${src}">
-            <div class="cart__product-count">${cartValue}</div>
-            </div>`
-    }
-
-    });
-
-};
