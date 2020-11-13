6761d58f54c2247ff47328331f805e851bf84564
Marina Osborne <65596875+MarinaOsborne@users.noreply.github.com>
2020-11-13 16:24:47 +0300

    Update task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,19 +1,33 @@
-'use strict'
-const interestСheck = document.getElementsByClassName('interest__check');
+ `use strict`
 
-for (let i = 0; i < interestСheck.length; i++) {
+const tooltips = [...document.querySelectorAll('.has-tooltip')];
+let active;
 
-  interestСheck[i].addEventListener('change', event => {  
-    const target = event.target.parentElement;  
-    const children = Array.from(target.nextElementSibling.querySelectorAll('.interest__check'));
-    for (let i = 0; i < interestСheck.length; i++) {
 
-       if (event.target.checked == true) {
-           children[i].checked = true
-           } else {
-             children[i].checked = false
-           }
-        }  
-    })
+function showTip(elem) {
+  
+  if (active = document.querySelector('.tooltip')) {
+    if (active === elem.nextElementSibling) {
+      active.remove();
+      return;
+    } else 
+    active.remove();
+  }
+
+
+  let tipText = elem.getAttribute('title');
+  elem.insertAdjacentHTML("afterEnd", `<div class="tooltip" style="left: 0; top: 0">${tipText}</div>`);
+  active = document.querySelector('.tooltip');
+  active.style.display = 'block';
+  active.style.left = elem.getBoundingClientRect().left + 'px';
+  active.style.top = elem.getBoundingClientRect().bottom + 'px'
+  
 }
+
+tooltips.forEach(item => {
+  item.addEventListener('click', e =>{
+    e.preventDefault();
+    showTip(item);
+  });
+});
 
